15400b3e676f3d60055a1db163a3e95ac8a525ae
AbhiDroid <abhinabroy2001@gmail.com>
2021-04-01 04:47:01 +0530

    Make Search page js

---
+++ page3/page3.js
@@ -0,0 +1,23 @@
+const navSlider = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle nav class
+        nav.classList.toggle('nav-active');
+
+        // Toggle animation
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } 
+            else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.4}s`;
+            }
+        }) 
+        burger.classList.toggle('toggle');
+    })
+}
+
+navSlider();
